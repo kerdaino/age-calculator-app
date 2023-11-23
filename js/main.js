c09b72c6ec59f8d7697faa67dd1253b244eeb935
kerdaino <kerdaino3@gmail.com>
2023-11-23 05:50:10 -0800

    third

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
+// Wait for the HTML document to be fully loaded before executing the JavaScript
 document.addEventListener('DOMContentLoaded', function () {
+  // Get references to various elements in the HTML
   const form = document.getElementById('ageForm');
   const calculateButton = document.getElementById('calculateButton');
   const inputs = document.querySelectorAll('.input');
   const ageResults = document.querySelectorAll('section h2 span');
   const header = document.querySelector('header');
 
-  // Add event listener to the entire document
+  // Add a click event listener to the entire document
   document.addEventListener('click', function (event) {
     // Check if the clicked element is not the calculate button or any input field
     if (event.target !== calculateButton && !Array.from(inputs).includes(event.target)) {
@@ -18,13 +20,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       });
 
+      // Reset header text color
       header.querySelectorAll('h1').forEach(h1 => {
         h1.style.color = 'hsl(0, 0%, 8%)';
       });
     }
   });
 
-
+  // Add a click event listener to the calculate button
   calculateButton.addEventListener('click', function (event) {
     event.preventDefault();
 
@@ -48,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if (input.value.trim() === '') {
         input.classList.add('error');
         showErrorMessage(input, 'This field is required.');
+        // Change header text color for error
         header.querySelectorAll('h1').forEach(h1 => {
           h1.style.color = 'hsl(0, 100%, 67%)';
         });
@@ -61,13 +65,16 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
 
+    // Parse input values for day, month, and year
     const day = parseInt(inputs[0].value, 10);
     const month = parseInt(inputs[1].value, 10);
     const year = parseInt(inputs[2].value, 10);
 
+    // Validate day, month, and year
     if (isNaN(day) || isNaN(month) || isNaN(year) || day < 1 || day > 31 || month < 1 || month > 12) {
       isValid = false;
 
+      // Highlight input fields with errors
       inputs.forEach((input, index) => {
         if (isNaN(parseInt(input.value, 10))) {
           input.classList.add('error');
@@ -85,11 +92,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
 
+    // Get the current date and the input date
     const currentDate = new Date();
     const inputDate = new Date(year, month - 1, day);
 
+    // Check if the input date is in the future
     if (inputDate > currentDate) {
       isValid = false;
+
+      // Show error messages and change header text color for date errors
       if (isNaN(day) || day < 1 || day > 31) {
         showErrorMessage(inputs[0], 'Must be a valid day.');
         header.querySelectorAll('h1').forEach(h1 => {
@@ -104,6 +115,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
 
+    // If the form is valid, calculate and display the age
     if (isValid) {
       // Calculate age
       const ageInMilliseconds = currentDate - inputDate;
@@ -119,6 +131,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  // Add a submit event listener to the form
   form.addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -126,6 +139,7 @@ document.addEventListener('DOMContentLoaded', function () {
     calculateButton.click();
   });
 
+  // Function to show error messages below input fields
   function showErrorMessage(input, message) {
     const errorMessage = document.createElement('div');
     errorMessage.textContent = message;
